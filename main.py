0b769b238a162f535bddc55510371e9b1a2fd0eb
Jakob Jung <jakob.jung@fau.de>
2021-07-20 13:00:22 +0200

    Changed from Chess to Connect

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import sys
 import ast
 from enum import Enum
 from datetime import datetime
-
-import chess
-import chess.pgn
+from connect4 import connect4
 import yaml
 from github import Github
 
@@ -15,7 +13,8 @@ import src.markdown as markdown
 import src.selftest as selftest
 import src.mockGithub as mockGithub
 
-# TODO: Use an image instead of a raw link to start new games
+RED = 1
+YELLOW  = 2
 
 class Action(Enum):
     UNKNOWN = 0
@@ -77,7 +76,7 @@ def parse_issue(title):
 
 def main(issue, issue_author, repo_owner):
     action = parse_issue(issue.title)
-    gameboard = chess.Board()
+    Conn = connect4()
 
     with open('data/settings.yaml', 'r') as settings_file:
         settings = yaml.load(settings_file, Loader=yaml.FullLoader)
@@ -95,55 +94,44 @@ def main(issue, issue_author, repo_owner):
             last_moves.write('Start game: ' + issue_author)
 
         # Create new game
-        game = chess.pgn.Game()
-        game.headers['Event'] = repo_owner + '\'s Online Open Connect4 Tournament'
-        game.headers['Site'] = 'https://github.com/' + os.environ['GITHUB_REPOSITORY']
-        game.headers['Date'] = datetime.now().strftime('%Y.%m.%d')
-        game.headers['Round'] = '1'
+        Conn.create_newgame()
+        #game.headers['Event'] = repo_owner + '\'s Online Open Connect4 Tournament'
+        #game.headers['Site'] = 'https://github.com/' + os.environ['GITHUB_REPOSITORY']
+        #game.headers['Date'] = datetime.now().strftime('%Y.%m.%d')
+        #game.headers['Round'] = '1'
 
     elif action[0] == Action.MOVE:
-        if not os.path.exists('games/current.pgn'):
+        if not os.path.exists('games/current.p'):
             return False, 'ERROR: There is no game in progress! Start a new game first'
 
         # Load game from "games/current.pgn"
-        with open('games/current.pgn') as pgn_file:
-            game = chess.pgn.read_game(pgn_file)
-            gameboard = game.board()
-
+        Conn = connect4()
+        Valid_Moves = Conn.valid_moves()
         with open('data/last_moves.txt') as moves:
             line = moves.readline()
             last_player = line.split(':')[1].strip()
             last_move   = line.split(':')[0].strip()
 
-        for move in game.mainline_moves():
-            gameboard.push(move)
 
-        # Try to move with promotion to queen
-        if chess.Move.from_uci(action[1] + 'q') in gameboard.legal_moves:
-            action = (action[0], action[1] + 'q')
-
-        move = chess.Move.from_uci(action[1])
 
         # Check if player is moving twice in a row
         if last_player == issue_author and 'Start game' not in last_move:
             issue.create_comment(settings['comments']['consecutive_moves'].format(author=issue_author))
             issue.edit(state='closed', labels=['Invalid'])
             return False, 'ERROR: Two moves in a row!'
-
+        move = issue
         # Check if move is valid
-        if move not in gameboard.legal_moves:
+        if move not in Valid_Moves:
             issue.create_comment(settings['comments']['invalid_move'].format(author=issue_author, move=action[1]))
             issue.edit(state='closed', labels=['Invalid'])
             return False, 'ERROR: Move is invalid!'
 
         # Check if board is valid
-        if not gameboard.is_valid():
+        if not Conn.has_space_left():
             issue.create_comment(settings['comments']['invalid_board'].format(author=issue_author))
             issue.edit(state='closed', labels=['Invalid'])
             return False, 'ERROR: Board is invalid!'
 
-        issue_labels = ['⚔️ Capture!'] if gameboard.is_capture(move) else []
-        issue_labels += ['White' if gameboard.turn == chess.WHITE else 'Black']
 
         issue.create_comment(settings['comments']['successful_move'].format(author=issue_author, move=action[1]))
         issue.edit(state='closed', labels=issue_labels)
@@ -152,42 +140,15 @@ def main(issue, issue_author, repo_owner):
         update_top_moves(issue_author)
 
         # Perform move
-        gameboard.push(move)
-        game.end().add_main_variation(move, comment=issue_author)
-        game.headers['Result'] = gameboard.result()
+        Conn.move(move)
+
 
     elif action[0] == Action.UNKNOWN:
         issue.create_comment(settings['comments']['unknown_command'].format(author=issue_author))
         issue.edit(state='closed', labels=['Invalid'])
         return False, 'ERROR: Unknown action'
 
-    # Save game to "games/current.pgn"
-    print(game, file=open('games/current.pgn', 'w'), end='\n\n')
 
-    last_moves = markdown.generate_last_moves()
-
-    # If it is a game over, archive current game
-    if gameboard.is_game_over():
-        win_msg = {
-            '1/2-1/2': 'It\'s a draw',
-            '1-0': 'White wins',
-            '0-1': 'Black wins'
-        }
-
-        with open('data/last_moves.txt', 'r') as last_moves:
-            lines = last_moves.readlines()
-            pattern = re.compile('.*: (@[a-z\\d](?:[a-z\\d]|-(?=[a-z\\d])){0,38})', flags=re.I)
-            player_list = { re.match(pattern, line).group(1) for line in lines }
-
-        issue.add_to_labels('👑 Winner!')
-        issue.create_comment(settings['comments']['game_over'].format(
-            outcome=win_msg.get(gameboard.result(), 'UNKNOWN'),
-            players=', '.join(player_list),
-            num_moves=len(lines)-1,
-            num_players=len(player_list)))
-
-        os.rename('games/current.pgn', datetime.now().strftime('games/game-%Y%m%d-%H%M%S.pgn'))
-        os.remove('data/last_moves.txt')
 
     with open('README.md', 'r') as file:
         readme = file.read()
@@ -200,9 +161,8 @@ def main(issue, issue_author, repo_owner):
     with open('README.md', 'w') as file:
         # Write new board & list of movements
         file.write(readme.format(
-            chess_board=markdown.board_to_markdown(gameboard),
-            moves_list=markdown.generate_moves_list(gameboard),
-            turn=('white' if gameboard.turn == chess.WHITE else 'black'),
+            chess_board=markdown.board_to_markdown(Conn.grid),
+            turn=('red' if Conn.whosturn() == RED else 'yellow'),
             last_moves=last_moves,
             top_moves=markdown.generate_top_moves()))
 
@@ -222,4 +182,4 @@ if __name__ == '__main__':
     ret, reason = main(issue, issue_author, repo_owner)
 
     if ret == False:
-        sys.exit(reson)
+        sys.exit(reason)
