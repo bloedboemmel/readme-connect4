3a7d4446f92c14104ff678f62b8313bcd211b6cd
Jakob Jung <jakob.jung@fau.de>
2021-07-20 15:02:27 +0200

    Removed twice in a row rule

--- main.py
+++ main.py
@@ -116,10 +116,10 @@ def main(issue, issue_author, repo_owner):
 
 
         # Check if player is moving twice in a row
-        if last_player == issue_author and 'Start game' not in last_move:
-            issue.create_comment(settings['comments']['consecutive_moves'].format(author=issue_author))
-            issue.edit(state='closed', labels=['Invalid'])
-            return False, 'ERROR: Two moves in a row!'
+        #if last_player == issue_author and 'Start game' not in last_move:
+        #    issue.create_comment(settings['comments']['consecutive_moves'].format(author=issue_author))
+        #    issue.edit(state='closed', labels=['Invalid'])
+        #    return False, 'ERROR: Two moves in a row!'
         move = action[1]
         # Check if move is valid
         if move not in Valid_Moves:
